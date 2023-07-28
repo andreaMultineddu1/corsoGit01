84e035c3d36cd5cfc138411533a0ed0ec9c05593
Andrea <andrea.multineddu.1@gmail.com>
2023-07-28 19:22:39 +0200

    Aggiunta funzione di divisione con controllo

--- function.php
+++ function.php
@@ -15,4 +15,15 @@ function moltiplicazione (n1,n2)
     return n1*n2;
 }
 
+function divisione (n1,n2)
+{
+    if(n2 != 0)
+    {
+        return n1/n2;
+    } else {
+        return "Error";
+    }
+    
+}
+
 ?>
